c3dc83c5742d3406f08f296b353161ab923551fe
Helen Langridge <langridge.helen@outlook.com>
2022-10-24 14:04:23 +0100

    can get all employees

--- src/main/java/com/codeclan/labday2/controllers/EmployeeController.java
+++ src/main/java/com/codeclan/labday2/controllers/EmployeeController.java
@@ -1,4 +1,23 @@
 package com.codeclan.labday2.controllers;
 
+import com.codeclan.labday2.models.Employee;
+import com.codeclan.labday2.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("employees")
 public class EmployeeController {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @GetMapping(value="")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
 }
