ebe62c057ac08f21913f22d5fb9f44c116fb29a3
Helen Langridge <langridge.helen@outlook.com>
2022-10-24 13:45:27 +0100

    setup with model

---
+++ src/main/java/com/codeclan/labday2/Labday2Application.java
@@ -0,0 +1,13 @@
+package com.codeclan.labday2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labday2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Labday2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/labday2/Labday2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.labday2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labday2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
